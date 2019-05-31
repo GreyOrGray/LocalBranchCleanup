a749c3532cd7bdbaed50fce12b7d6821f7fc72ed
Gray <graybeeman@gmail.com>
2019-05-31 10:42:02 -0600

    Revision
    Updated formatting for better readability and standard look
    
    Changed while to switch for less clutter

--- BranchCleanup.ps1
+++ BranchCleanup.ps1
@@ -11,26 +11,24 @@ the selections.
 Once confirmation has been given, the selected branches are deleted and the
 branches are compared again, presenting a final output of differences if any.
 
-If the directory chosen is NOT a valid repo clone, the script should return
-an error message saying as much and quit out. 
-
-If there are no differences, the function will say as much and exit.
-
 .PARAMETER Path
 String representing the local path for the repo clone. If this is not
 provided, the function assumes the repo clone is meant to be the current
 directory. 
+
+.NOTES
+Dependent on the function Out-HostColored
+
+If the directory chosen is NOT a valid repo clone, the script should return
+an error message saying as much and quit out. 
+
+If there are no differences, the function will say as much and exit.
 
 .EXAMPLE
 BranchCleanup 'F:\STASH\db_salesforcebackups_repo'
 
 sets the location to the path provided and runs the compare and options
 to delete branches if applicable.
-
-.NOTES
-Author: GreyOrGray
-Requires: Out-HostColored
-
 
 #>
 function BranchCleanup
@@ -41,23 +39,27 @@ function BranchCleanup
         [String] $path = $(get-location).path
     ) 
 
-	write-host ("***********************************************************************************" ) -f green
-	write-host ("Branch Cleanup Starting" ) -f green
-	write-host ("***********************************************************************************`n" ) -f green
-	write-host ("Changing directory to {0}`n" -f ($path)) -f yellow
+	$lineBreak = "***********************************************************************************"
+	
+	write-host ("`n{0}`nBranch Cleanup Starting`n{1}`n" -f ($linebreak, $linebreak)) -f cyan
+	
+	write-host ("Changing directory to {0}`n" -f ($path)) -f green
 	set-location $path
 	
-	write-host ("Collecting local branch info`n") -f yellow
+	write-host ("Collecting local branch info: ") -f green -nonewline
 	try{
 			$local=git branch -l
 			if (-not $?) {throw 'error with git connection'}
-		} catch { write-host  ("Error collecting local branch info`n`n") -f red; RETURN}
-	write-host ("Collecting remote branch info`n") -f yellow
+			write-host ("{0} local branch{1} found`n" -f ($local.count, $(if ($local.count -gt 1){'es'}else{''}))) -f yellow 
+			} catch { write-host  ("Error collecting local branch info`n`n") -f red; RETURN}
+		
+	write-host ("Collecting remote branch info: ") -f green -nonewline
 	try{
 			$remote=git branch -r
 			if (-not $?) {throw 'error with git connection'}
+			write-host ("{0} remote branch{1} found`n" -f ($remote.count, $(if ($remote.count -gt 1){'es'}else{''}))) -f yellow 
 		} catch { write-host  ("Error collecting remote branch info`n`n") -f red; RETURN}
-	write-host ("Comparing branches`n") -f yellow
+	write-host ("Comparing branches`n") -f green
 	$comparisonValues = $local | %{$_.Trim()}|?{-not ($remote -like '*' + ($_ -replace "^[\\* ]",'').trim()) }|?{-not($_ -match "master")}
 	
  
@@ -79,89 +81,96 @@ function BranchCleanup
 	
 	if ($branchTable.rows.count -eq 0)
 	{ 
-		write-host ("No local branches have been identified as not existing on the remote`n`n") -f green 
+		write-host ("`n{0}`nNo local branches have been identified as not existing on the remote`n`nGoodbye.`n{1}`n" -f ($linebreak, $linebreak)) -f cyan
 		return
 	}
 	
 	$Accept = $False
-	while ($Accept -eq $false)
-	{
+	
+	do {
 		$Confirm = $False
 		write-host ("***The following local branches have been identified as not existing on the remote***") -f yellow
-		#$hash.keys | select @{l='ID';e={$_}},@{l='Branch';e={$hash.$_}} | sort ID
 		$branchTable | ft -auto | out-hostcolored 'True' red
 		
-		write-host ("***********************************************************************************`n" ) -f green
-		Write-Host 'Please provide one of the following' -ForegroundColor green
-		Write-Host '    - A comma delimited list of Branch IDs for which to change the "DELETE" flag' -ForegroundColor green
-		Write-Host '    - "A" to select all ' -ForegroundColor green
-		Write-Host '    - "Q" to quit: ' -ForegroundColor green -NoNewline
+		write-host ("`n{0}`nPlease provide one of the following" -f ($linebreak)) -f cyan
+		Write-Host '    - A comma delimited list of Branch IDs for which to change the "DELETE" flag' -ForegroundColor cyan
+		Write-Host '    - "A" to select all ' -ForegroundColor cyan
+		Write-Host '    - "Q" to quit' -ForegroundColor cyan 
+		Write-Host 'Your Choice: ' -ForegroundColor green -NoNewline
+		
 		$userInput = Read-Host
 		
-		$splitInput = $userInput.trim() -split(',')
 		
 		
-		Write-Host "`n`n"
-		if ($splitInput -notmatch '^[aq0-9]+$')
+		switch -regex ($userInput)
 		{
-			write-host ("***********************************************************************************`n" ) -f red
-			Write-Host 'Your input contains an invalid value.' -ForegroundColor red
-			Write-Host "Please try again.`n`n" -ForegroundColor red
-		} elseif ($splitInput -match '[qQ]')
-		{
-			Write-Host 'You have chosen to quit.' -ForegroundColor red
-			Write-Host 'Goodbye.' -ForegroundColor red
-			RETURN			
-		} elseif ($splitInput -match '[aA]')
-		{
-			Write-Host 'You have chosen to select all.' -ForegroundColor yello
-			$branchTable |  foreach {$_.DELETE = !$_.DELETE}
-			$Confirm = $True
-		} else
-		{
-			foreach ($splitVal in $splitInput)
+			'[^aq0-9,]+$'
 			{
-				$branchTable | where {$_.ID -eq $splitVal}  | foreach {$_.DELETE = !$_.DELETE}
+				write-host ("`n{0}`nYour input contains an invalid value.`nPlease try again.`n`n{1}`n" -f ($linebreak, $linebreak)) -ForegroundColor red
 			}
-			$Confirm = $True
+			'q'
+			{
+				write-host ("`n{0}`nYou have chosen to quit.`n`nGoodbye.`n{1}`n" -f ($linebreak, $linebreak)) -ForegroundColor cyan
+				return
+			}
+			'a'
+			{
+				Write-Host ("`n{0}`nYou have chosen to select all.`n{1}`n" -f ($linebreak, $linebreak)) -ForegroundColor cyan
+				$branchTable |  foreach {$_.DELETE = !$_.DELETE}
+				$Confirm = $True
+			}
+			default
+			{
+				Write-Host ("`n{0}`nYou have chosen: {1} `n{2}`n" -f ($linebreak,  $userInput.trim(), $linebreak)) -ForegroundColor cyan
+				$splitInput = $userInput.trim() -split(',')
+				foreach ($splitVal in $splitInput)
+				{
+					$branchTable | where {$_.ID -eq $splitVal}  | foreach {$_.DELETE = !$_.DELETE}
+				}
+				$Confirm = $True
+			}
+			
 		}
 		if ($Confirm -eq $True)
-		{
-			
-			write-host ("***********************************************************************************`n" ) -f green
-			Write-Host 'Your current selection...' -ForegroundColor green
-			$branchTable | ft -auto | out-hostcolored 'True' red
-			Write-Host 'Enter "Y" to procede with the chosen branch deletion or any other value to go back and change your choice.' -ForegroundColor green
-			$userInput = Read-Host
-			Write-Host "`n`n`n"
-
-			if ($userInput -match '[y]')
 			{
-				$Accept = $True
+				
+				Write-Host "`nYour current selection..." -ForegroundColor yellow
+				$branchTable | ft -auto | out-hostcolored 'True' red
+				write-host ("`n{0}`nEnter ""Y"" to procede with the chosen branch deletion or enter any other value to go back and change your choice." -f ($linebreak)) -f cyan
+				$userInput = Read-Host
+				Write-Host "`n`n`n"
+
+				if ($userInput -match '[y]')
+				{
+					$Accept = $True
+				}
 			}
-		}
-	}
+	} while ($Accept -eq $false)
 	
-	write-host 'Accepted...' -f yellow
+	write-host ("`n{0}`nACCEPTED`n{1}`n" -f ($linebreak, $linebreak)) -ForegroundColor cyan
 
 	foreach ($row in $branchTable.rows | ?{$_.DELETE -eq $True})
 	{
-		write-host ('Deleting branch : {0}' -f ($row.Branch)) -f yellow
+		write-host ('Deleting branch : {0}' -f ($row.Branch)) -ForegroundColor cyan
 		git branch -D $row.Branch
+		write-host "`n"
 	}
 	
-	write-host ("Re-collecting local branch info`n") -f yellow
+	write-host ("Re-collecting local branch info: ") -ForegroundColor green -nonewline
 	try{
 			$local=git branch -l
 			if (-not $?) {throw 'error with git connection'}
-		} catch { write-host  ("Error collecting local branch info`n`n") -f red; RETURN}
-	write-host ("Re-collecting remote branch info`n") -f yellow
+			write-host ("{0} local branch{1} found`n" -f ($local.count, $(if ($local.count -gt 1){'es'}else{''}))) -ForegroundColor yellow 
+		} catch { write-host  ("Error collecting local branch info`n`n") -ForegroundColor red; RETURN}
+	
+	write-host ("Re-collecting remote branch info: ") -ForegroundColor green -nonewline
 	try{
 			$remote=git branch -r
 			if (-not $?) {throw 'error with git connection'}
-		} catch { write-host  ("Error collecting remote branch info`n`n") -f red; RETURN}
-	write-host ("Re-comparing branches`n") -f yellow
-	$comparisonValues = $local | %{$_.Trim()}|?{-not ($remote -like '*' + $_) }|?{-not($_ -match "master")}
+			write-host ("{0} remote branch{1} found`n" -f ($remote.count, $(if ($remote.count -gt 1){'es'}else{''}))) -ForegroundColor yellow 
+		} catch { write-host  ("Error collecting remote branch info`n`n") -ForegroundColor red; RETURN}
+	write-host ("Re-comparing branches`n") -ForegroundColor green
+	$comparisonValues = $local | %{$_.Trim()}|?{-not ($remote -like '*' + ($_ -replace "^[\\* ]",'').trim()) }|?{-not($_ -match "master")}
 
 	$branchTable = New-Object system.Data.DataTable "BranchTable"
 	$col1 = New-Object system.Data.DataColumn ID,([int])
@@ -178,9 +187,11 @@ function BranchCleanup
 	
 	if ($branchTable.rows.count -gt 0)
 	{
-		write-host ("The following local branches have been identified as not existing on the remote") -f yellow
+		write-host ("***The following local branches have been identified as not existing on the remote***") -ForegroundColor yellow
 		$branchTable | ft -auto
-	} else { write-host ("No local branches have been identified as not existing on the remote") -f green }
+		write-host ("`n{0}`nEnd of Branch Cleanup.`n`nGoodbye.`n{1}`n" -f ($linebreak, $linebreak)) -ForegroundColor cyan 
+	} else { write-host ("`n{0}`nNo local branches have been identified as not existing on the remote`n`nGoodbye.`n{1}`n" -f ($linebreak, $linebreak)) -ForegroundColor cyan }
+	
 	
 	
 } 
